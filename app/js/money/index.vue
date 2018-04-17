4a8aa79975c867d5691e3d6bbbb5efa09a1be723
Ms_tian <tian_chao163@163.com>
2018-04-17 21:42:11 +0800

    理财页面的初始化 -03

---
+++ app/js/money/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div title="首页">
+    <heador/>
+    
+    <Footor/>
+    <Navbar/>
+  </div>
+</template>
+
+<script > 
+  import heador from "../public/header.vue";
+  import Footor from "../public/footer.vue";
+  import Navbar from "../public/navbar.vue";
+export default {
+ 
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    heador,
+    
+    Footor,
+    Navbar,
+  }
+}
+</script>
+
--- app/js/public/navbar.vue
+++ app/js/public/navbar.vue
@@ -1,11 +1,29 @@
 <template lang="html">
   <ul title="导航" :class="$style.navs">
-      <li v-for="item in items">
+        <li>
           <router-link :to="{ name: 'home'}">
-            <img :src="item.src" :alt="item.title" >
-            <p>{{item.title}}</p>
+            <img src="//img12.360buyimg.com/jrpmobile/jfs/t3541/110/627394787/2190/5d02d39c/580f1dfdNcf72af31.png?width=66&height=66" >
+            <p>首页</p>
           </router-link>
-      </li>
+        </li>
+        <li>
+          <router-link :to="{ name: 'money'}">
+            <img src="//img12.360buyimg.com/jrpmobile/jfs/t3811/55/345433999/2930/116d3dd1/580f1e25N028a1f2c.png?width=66&height=66">
+            <p>理财</p>
+          </router-link>
+        </li>
+        <li>
+          <router-link :to="{ name: 'home'}">
+            <img src="//img12.360buyimg.com/jrpmobile/jfs/t3343/179/621044750/1484/5af6b11a/580f50e4N005181d0.png?width=66&height=66" >
+            <p>白条</p>
+          </router-link>
+        </li>
+        <li>
+          <router-link :to="{ name: 'home'}">
+            <img src="//img12.360buyimg.com/jrpmobile/jfs/t3340/68/639059082/2591/800e2d6b/580f51cdN4caf20b2.png?width=66&height=66">
+            <p>我的</p>
+          </router-link>
+        </li>
   </ul>
 </template>
 
